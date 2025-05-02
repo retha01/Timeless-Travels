990b7d4d674defffee69843324678ff399f3040e
retha01 <162324350+retha01@users.noreply.github.com>
2025-05-03 01:49:04 +0200

    hi i added this user controller. lmk what you think.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿namespace Timeless_Travel.Controllers
+{
+    public class UserController
+    {
+        //here is some code
+
+        //here is more code
+
+        //and here is a few more lines of code
+    }
+}
